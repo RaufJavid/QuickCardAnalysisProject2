8452a1d6496707a84718e30a34b40789c5226c9b
Abdul Rauf <raufjavid5@gmail.com>
2019-10-06 15:47:30 +0500

    initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn QuickCardAnalysisProject.wsgi --log-file -
---
+++ QuickCardAnalysisApp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+# Create your views here.
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def index(request):
+
+    template = loader.get_template('QuickCardAnalysisApp/index.html')
+
+    # return HttpResponse(template.render(request))
+
+    return render(request,'QuickCardAnalysisApp/index.html')
